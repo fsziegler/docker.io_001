394bf085f514fc1877bafb90c6413ae1afb7c2a0
fsziegler <fsziegler@gmail.com>
2014-07-10 08:21:00 -0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,3 +1,31 @@
+#!/bin/sh
+#
+# Written 2014-09-20 by Fred Ziegler
+# Copyright © 2014, Frederick S. Ziegler
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Designed for Kubuntu Linux systems - should work on any Debian system.
+
+# Instructions: run `chmod +x install_docker.sh` from the command line, 
+#               then . ./install_docker.sh to run
+
 sudo apt-get update
 sudo apt-get install curl apt-transport-https
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
